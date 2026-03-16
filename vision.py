ea1fbbe5a7f656da041a872238168e062405e455
Spazmatolog <pavlobendas@gmail.com>
2026-03-16 15:23:13 +0200

    added tesseract

--- vision.py
+++ vision.py
@@ -2,6 +2,10 @@ import keyboard
 import pyautogui
 import cv2
 import numpy as np
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
 sizex, sizey = pyautogui.size()
 print("Screen size: " + str(sizex) + "x" + str(sizey))
 print()
@@ -12,7 +16,10 @@ cargo = pyautogui.screenshot(region=(x, y, 300, 300))
 cargo.save("screenshot/cargo.png")
 
 img = cv2.imread("screenshot/cargo.png", cv2.IMREAD_GRAYSCALE)
- 
-retval, result = cv2.threshold(img, 127, 255, cv2.THRESH_BINARY)
+
+img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2), interpolation=cv2.INTER_CUBIC)
+retval, result = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
 
 cv2.imwrite("screenshot/result.png", result)
+
+print(pytesseract.image_to_string('screenshot\\result.png'))
