414406e5a53e9d34909f671b10d3bd01f7e4abb1
Spazmatolog <pavlobendas@gmail.com>
2026-03-19 08:37:27 +0200

    bebebebebe

--- vision.py
+++ vision.py
@@ -5,21 +5,34 @@ import numpy as np
 import pytesseract
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
+# цены на руды чегоо
+prices = {
+    "veldspar": 12.5,
+    "scordite": 15.0,
+    "pyroxeres": 45.0
+}
+
 
 sizex, sizey = pyautogui.size()
 print("Screen size: " + str(sizex) + "x" + str(sizey))
 print()
+
 keyboard.wait('p')
 
 x, y = pyautogui.position()
-cargo = pyautogui.screenshot(region=(x, y, 300, 300))
+cargo = pyautogui.screenshot(region=(x, y, 200, 30))
 cargo.save("screenshot/cargo.png")
 
 img = cv2.imread("screenshot/cargo.png", cv2.IMREAD_GRAYSCALE)
 
 img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2), interpolation=cv2.INTER_CUBIC)
-retval, result = cv2.threshold(img, 100, 255, cv2.THRESH_BINARY)
+retval, result = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
 
 cv2.imwrite("screenshot/result.png", result)
 
-print(pytesseract.image_to_string('screenshot\\result.png'))
+textresult = pytesseract.image_to_string("screenshot/result.png")
+textresult = textresult.strip().lower()
+print(repr(textresult))
+
+if textresult in prices:
+    print("Price: " + str(prices[textresult]) + " ISK/m3")
