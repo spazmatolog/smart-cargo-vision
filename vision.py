ab79a42aef1e4f91ef0895b54d9d76640f500904
Spazmatolog <pavlobendas@gmail.com>
2026-03-26 16:47:05 +0200

    while true

--- vision.py
+++ vision.py
@@ -16,23 +16,26 @@ prices = {
 sizex, sizey = pyautogui.size()
 print("Screen size: " + str(sizex) + "x" + str(sizey))
 print()
+while True:
+    keyboard.wait('p')
 
-keyboard.wait('p')
+    x, y = pyautogui.position()
+    cargo = pyautogui.screenshot(region=(x, y, 200, 30))
+    cargo.save("screenshot/cargo.png")
 
-x, y = pyautogui.position()
-cargo = pyautogui.screenshot(region=(x, y, 200, 30))
-cargo.save("screenshot/cargo.png")
+    img = cv2.imread("screenshot/cargo.png", cv2.IMREAD_GRAYSCALE)
 
-img = cv2.imread("screenshot/cargo.png", cv2.IMREAD_GRAYSCALE)
+    img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2), interpolation=cv2.INTER_CUBIC)
+    retval, result = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
 
-img = cv2.resize(img, (img.shape[1] * 2, img.shape[0] * 2), interpolation=cv2.INTER_CUBIC)
-retval, result = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
+    cv2.imwrite("screenshot/result.png", result)
 
-cv2.imwrite("screenshot/result.png", result)
+    textresult = pytesseract.image_to_string("screenshot/result.png")
+    textresult = textresult.strip().lower()
+    print(repr(textresult))
 
-textresult = pytesseract.image_to_string("screenshot/result.png")
-textresult = textresult.strip().lower()
-print(repr(textresult))
+    if textresult in prices:
+        print("Price: " + str(prices[textresult]) + " ISK/m3")
 
-if textresult in prices:
-    print("Price: " + str(prices[textresult]) + " ISK/m3")
+    if keyboard.is_pressed(']'):
+        break
